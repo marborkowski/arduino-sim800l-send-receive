090c41ddc169f69ea1fc5fb08eceb19d563c574e
Marcin Borkowski <marcin.borkowski@rb.com>
2016-06-27 09:07:18 +0200

    Parsing SMS events.

--- sim800l-sketch/sim800l-sketch.ino
+++ sim800l-sketch/sim800l-sketch.ino
@@ -100,8 +100,8 @@ String getValue(String data, char separator, int index)
   int strIndex[] = { 0, -1 };
   int maxIndex = data.length() - 1;
   
-  for(int i=0; i <= maxIndex && found <= index; i++){
-    if(data.charAt(i) == separator || i == maxIndex){
+  for(int i=0; i <= maxIndex && found <= index; i++) {
+    if(data.charAt(i) == separator || i == maxIndex) {
       found++;
       strIndex[0] = strIndex[1]+1;
       strIndex[1] = (i == maxIndex) ? i+1 : i;
